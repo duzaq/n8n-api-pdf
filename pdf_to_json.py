b866a3c762e2ba1b268e783b107cfc1ed6bb275b
duzaq <duzaq@live.com>
2024-12-31 10:02:51 -0300

    Update pdf_to_json.py
    V4

--- pdf_to_json.py
+++ pdf_to_json.py
@@ -1,16 +1,16 @@
-import pdfplumber
+import camelot
 import json
 import sys
 
-def pdf_to_json(pdf_path):
-    with pdfplumber.open(pdf_path) as pdf:
-        pages = [page.extract_text() for page in pdf.pages]
-    return json.dumps(pages, ensure_ascii=False)
+def pdf_table_to_json(pdf_path):
+    tables = camelot.read_pdf(pdf_path, pages='all')
+    table_data = [table.df.to_dict(orient='records') for table in tables]
+    return json.dumps(table_data, ensure_ascii=False)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python pdf_to_json.py <pdf_path>")
+        print("Usage: python pdf_table_to_json.py <pdf_path>")
         sys.exit(1)
     
     pdf_path = sys.argv[1]
-    print(pdf_to_json(pdf_path))
+    print(pdf_table_to_json(pdf_path))
